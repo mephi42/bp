b60b1e6f58b6c5914dfab222620e9cd5e945d6dc
mephi42 <mephi42@gmail.com>
2019-06-17 16:51:43 +0200

    Fix pattern generation

--- bp.c
+++ bp.c
@@ -35,13 +35,13 @@
 static void *alloc_pattern(const char *s, int repeat)
 {
 	size_t len = strlen(s);
-	const int count = len * repeat;
+	size_t count = len * repeat;
 	int *d = malloc(count * sizeof(int));
 	assert(d != NULL);
 	for (size_t i = 0; i < len; i++)
 		d[i] = s[i] == '0' ? 0 : 1;
-	for (int i = len; i < count; i += len)
-		memcpy(&d[i], &d[0], len);
+	for (size_t i = len; i < count; i += len)
+		memcpy(&d[i], &d[0], len * sizeof(int));
 	return d;
 }
 
